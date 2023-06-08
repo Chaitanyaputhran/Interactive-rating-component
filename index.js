d649a48dedaaa10089eb1301070ab78d7e19446d
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-06-08 22:49:28 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const sub=document.querySelector(".submit");
 const first=document.querySelector(".first-page");
 const second=document.querySelector(".second-page");
-const text=document.querySelector(".thanks")
+const text=document.querySelector(".thanks");
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.add('clicked');
+  });
+});
 second.classList.add("hide");
 let number
 
 function one(){
     number=1
+  
     return number
 }
 function two(){
@@ -28,7 +35,9 @@ function five(){
 sub.addEventListener("click",function(){
     second.classList.remove("hide");
     first.classList.add("hide");
-    text.innerHTML("")
+    text.innerText = "You selected"+" "+number+" "+"out of 5"
+    
+   
 
 })
 
