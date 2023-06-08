b66105b909c6bfc19af8035117817c84046f635e
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-06-08 21:30:21 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-var sub=document.querySelector(".submit");
+const sub=document.querySelector(".submit");
+const first=document.querySelector(".first-page");
+const second=document.querySelector(".second-page");
+const text=document.querySelector(".thanks")
+second.classList.add("hide");
+let number
+
+function one(){
+    number=1
+    return number
+}
+function two(){
+    number=2
+    return number
+}
+function three(){
+    number=3
+    return number
+}
+function four(){
+    number=4
+    return number
+}
+function five(){
+    number=5
+    return number
+}
+sub.addEventListener("click",function(){
+    second.classList.remove("hide");
+    first.classList.add("hide");
+    text.innerHTML("")
+
+})
+
+
+
